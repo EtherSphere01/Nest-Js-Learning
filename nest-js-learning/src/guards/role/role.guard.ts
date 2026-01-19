520b2b588ac88de0431331008be4eeab0d57a42a
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2026-01-20 01:27:05 +0600

    feat: implement role-based access control with RoleGuard and related files

---
+++ nest-js-learning/src/guards/role/role.guard.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { Role } from './role.enums';
+import { ROLES_KEY } from './role.decorator';
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) {
+      return true;
+    }
+    const request = context
+      .switchToHttp()
+      .getRequest<{ headers: Record<string, string> }>();
+    const userRole = request.headers['x-user-role'] as Role;
+
+    if (requiredRoles.includes(userRole)) {
+      return true;
+    }
+    return false;
+  }
+}
